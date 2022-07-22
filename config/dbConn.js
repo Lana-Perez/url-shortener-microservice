95e811fbc42269b8f22435dee72312647533855d
Lana-Perez <89758188+Lana-Perez@users.noreply.github.com>
2022-07-22 07:25:35 -0700

    dburl added

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-
+const dbUri = process.env.DATABASE_URI || "mongodb+srv://user:123test@cluster0.xsx34.mongodb.net/URLShortener?retryWrites=true&w=majority"
 //mongoose.set("debug", true);
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.DATABASE_URI);
+    await mongoose.connect(dbUri);
+
   } catch (err) {
     console.error(err);
   }
